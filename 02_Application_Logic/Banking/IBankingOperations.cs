b4a5334dc2e1c42795a58a9690fda97040623266
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:37:37 -0800

    Update IBankingOperations.cs

--- 02_Application_Logic/Banking/IBankingOperations.cs
+++ 02_Application_Logic/Banking/IBankingOperations.cs
@@ -1,48 +1,9 @@
 // -- filepath fintechs-exhibitu/02_Application_Logic/Banking/IBankingOperations.cs
-using GlobalBank.Domain.Interfaces;
-
-namespace GlobalBank.Application.Banking;
-
-public class BankingOperations : IBankingOperations
+namespace GlobalBank.Domain.Interfaces
 {
-    private readonly IBankingRepository _repo;
-
-    public BankingOperations(IBankingRepository repo)
+    public interface IBankingOperations
     {
-        _repo = repo;
-    }
-
-    public async Task<bool> TransactAsync(Guid from, Guid to, decimal amount)
-    {
-        if (amount <= 0)
-            return false;
-
-        var fromBalance = await _repo.GetTotalLedgerBalanceAsync(); // simplified for now
-        if (fromBalance < amount)
-            return false;
-
-        await _repo.InsertLedgerEntryAsync(
-            from,
-            credit: 0,
-            debit: amount,
-            description: "Transfer Out"
-        );
-
-        await _repo.InsertLedgerEntryAsync(
-            to,
-            credit: amount,
-            debit: 0,
-            description: "Transfer In"
-        );
-
-        return true;
-    }
-
-    public async Task<bool> ReconcileWithPhysicalVaultAsync()
-    {
-        var ledgerTotal = await _repo.GetTotalLedgerBalanceAsync();
-        var vaultTotal  = await _repo.GetTotalPhysicalVaultValueAsync();
-
-        return ledgerTotal == vaultTotal;
+        Task<bool> TransactAsync(Guid from, Guid to, decimal amount);
+        Task<bool> ReconcileWithPhysicalVaultAsync();
     }
 }
