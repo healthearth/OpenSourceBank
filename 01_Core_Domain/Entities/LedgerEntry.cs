4746dacc25d72488cbde33cf13666633a07fb6a6
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 10:09:52 -0800

    Update LedgerEntry.cs

--- 01_Core_Domain/Entities/LedgerEntry.cs
+++ 01_Core_Domain/Entities/LedgerEntry.cs
@@ -1,1 +1,8 @@
-
+// Filepath: fintechs-exhibitu/01_Core_Domain/Entities/LedgerEntry.cs
+public class LedgerEntry {
+    public Guid Id { get; } = Guid.NewGuid();
+    public decimal Debit { get; init; }  // Money Out
+    public decimal Credit { get; init; } // Money In
+    public string PhysicalAssetId { get; init; } // Link to physical cash/library asset
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
+}
