d087ea197e7db5d8f9b78d0d09d9a7f0b1cdce9a
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 12:47:37 -0800

    Update Account.cs
    removed private parameter

--- 01_Core_Domain/Entities/Account.cs
+++ 01_Core_Domain/Entities/Account.cs
@@ -2,10 +2,10 @@
 namespace GlobalBank.Domain.Entities;
 
 public class Account {
-    public Guid Id { get; private set; }
+    public Guid Id { get; set; }
     public string OwnerName { get; private set; }
-    public string CurrencyCode { get; private set; } // "AI$"
-    public decimal DigitalBalance { get; private set; }
+    public string CurrencyCode { get; set; } = "USD";
+    public decimal Balance { get; set; }
     public bool IsKycVerified { get; private set; }
 
     public Account(string owner, string currency) {
