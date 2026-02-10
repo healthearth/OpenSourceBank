0a7947aefdf7993af5293946a1cc509efdbefb70
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 16:38:36 -0800

    Update LedgerEntry.cs
    Included public Guid? PhysicalAssetId { get; set; }

--- 01_Core_Domain/Entities/LedgerEntry.cs
+++ 01_Core_Domain/Entities/LedgerEntry.cs
@@ -5,5 +5,6 @@ public class LedgerEntry {
     public decimal Credit { get; init; } // Money In
     public string Description { get; set; } = string.Empty;
     public string PhysicalAssetId { get; init; } // Link to physical cash/library asset
+    public Guid? PhysicalAssetId { get; set; }
     public DateTime Timestamp { get; } = DateTime.UtcNow;
 }
