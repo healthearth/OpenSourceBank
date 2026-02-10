f9daa5655e73a5de2794432e6cb8ed3cc8756f49
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 17:42:11 -0800

    Update LedgerEntry.cs
    included namespace GlobalBank.Domain.Entities;
    changed set to init spacing

--- 01_Core_Domain/Entities/LedgerEntry.cs
+++ 01_Core_Domain/Entities/LedgerEntry.cs
@@ -1,10 +1,12 @@
 // Filepath: fintechs-exhibitu/01_Core_Domain/Entities/LedgerEntry.cs
+namespace GlobalBank.Domain.Entities;
+    
 public class LedgerEntry {
     public Guid Id { get; } = Guid.NewGuid();
     public decimal Debit { get; init; }  // Money Out
     public decimal Credit { get; init; } // Money In
-    public string Description { get; set; } = string.Empty;
-    public string PhysicalAssetId { get; init; } // Link to physical cash/library asset
-    public Guid? PhysicalAssetId { get; set; }
-    public DateTime Timestamp { get; } = DateTime.UtcNow;
+    public string Description { get; init; } = string.Empty;
+    public string PhysicalAssetRef { get; init; } // Link to physical cash/library asset
+    public string? PhysicalAssetRef { get; init; }
+    public DateTime TimestampUtc { get; } = DateTime.UtcNow;
 }
