1c83c3b963148eeb6ba4a132be80bc601d84f2c2
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 18:13:57 -0800

    Update ExchangeEngine.cs
    Added  public static decimal ConvertToAiDollar(decimal amount, string currencyCode)
            => Convert(amount, currencyCode);

--- 01_Core_Domain/Services/ExchangeEngine.cs
+++ 01_Core_Domain/Services/ExchangeEngine.cs
@@ -5,4 +5,7 @@ public static class ExchangeEngine
 {
     public static decimal Convert(decimal amount, string currencyCode)
         => amount; // placeholder until real FX logic
+
+    public static decimal ConvertToAiDollar(decimal amount, string currencyCode)
+        => Convert(amount, currencyCode);
 }
