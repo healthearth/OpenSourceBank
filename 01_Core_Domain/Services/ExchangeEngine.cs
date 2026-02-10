85acb654670f519ace6c18ff36a88e8203398580
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 16:03:45 -0800

    Create ExchangeEngine.cs

---
+++ 01_Core_Domain/Services/ExchangeEngine.cs
@@ -0,0 +1,8 @@
+// fintechs-exhibitu/01_Core_Domain/Services/ExchangeEngine.cs
+namespace GlobalBank.Domain.Services;
+
+public static class ExchangeEngine
+{
+    public static decimal Convert(decimal amount, string currencyCode)
+        => amount; // placeholder
+}
