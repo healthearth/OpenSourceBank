258ca234b54f215d5e36bc017c939438bac7d4d3
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-09 18:04:16 -0800

    Update LedgerEntry.cs
    removed extra line

--- 01_Core_Domain/Entities/LedgerEntry.cs
+++ 01_Core_Domain/Entities/LedgerEntry.cs
@@ -1,12 +1,12 @@
 // Filepath: fintechs-exhibitu/01_Core_Domain/Entities/LedgerEntry.cs
 namespace GlobalBank.Domain.Entities;
     
-public class LedgerEntry {
+public class LedgerEntry 
+{
     public Guid Id { get; } = Guid.NewGuid();
     public decimal Debit { get; init; }  // Money Out
     public decimal Credit { get; init; } // Money In
     public string Description { get; init; } = string.Empty;
-    public string PhysicalAssetRef { get; init; } // Link to physical cash/library asset
-    public string? PhysicalAssetRef { get; init; }
+    public string? PhysicalAssetRef { get; init; } // Link to physical cash/library asset
     public DateTime TimestampUtc { get; } = DateTime.UtcNow;
 }
