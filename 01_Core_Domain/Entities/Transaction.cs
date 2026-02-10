9d5220a3803f43217989c542f7312df35c6cdb77
Repository Gang <178992666+repositorgan@users.noreply.github.com>
2026-02-10 15:14:35 -0800

    Update Transaction.cs

--- 01_Core_Domain/Entities/Transaction.cs
+++ 01_Core_Domain/Entities/Transaction.cs
@@ -1,4 +1,5 @@
 // Filepath: fintechs-exhibitu/01_Core_Domain/Entities/Transaction.cs
+// © 2026 Andrew Kieckhefer. All rights reserved.
 namespace GlobalBank.Domain.Entities;
 
 public class Transaction {
