1cbca92fea659db0a51ed5e1811044db075c1504
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-11 23:53:16 -0800

    Update Program.cs
    BTC deposit accounted for.

--- Program.cs
+++ Program.cs
@@ -35,3 +35,26 @@ await service.RegisterStartupCapitalAsync(
     targetAccountId: foundersCapitalAccountId,
     assets: new[] { oneOSB }
 );
+
+var costaRicaGoldCoin = new PhysicalAssetDeposit( 
+    faceValue: 500m, // 500 CRC 
+    currencyCode: "CRC", 
+    serialNumber: "CRC-COIN-500-20260212-001R" // generated unique ID
+); 
+
+await service.RegisterStartupCapitalAsync( 
+    targetAccountId: foundersCapitalAccountId, 
+    assets: new[] { costaRicaGoldCoin } 
+);
+
+var btcDeposit = new PhysicalAssetDeposit(
+    faceValue: 0.00010521m,              // 0.00010521 BTC
+    currencyCode: "BTC",
+    serialNumber: "BTC-WALLET-KEY-20260212-001" // internal unique ID
+);
+
+await service.RegisterStartupCapitalAsync(
+    targetAccountId: foundersCapitalAccountId,
+    assets: new[] { btcDeposit }
+);
+
