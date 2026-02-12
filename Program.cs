fb3cc203a05f4cf2d668d70b5755b1582bc5200c
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-11 23:40:10 -0800

    Update Program.cs
    Include foundry global currencies

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
 // fintechs-exhibitu/Program.cs
+
+var tenDollars = new PhysicalAssetDeposit(
+    faceValue: 10m,
+    currencyCode: "USD",
+    serialNumber: "PK25146493B"
+);
+
+var service = new CapitalRegistrationService(repo);
+
+await service.RegisterStartupCapitalAsync(
+    targetAccountId: foundersCapitalAccountId,
+    assets: new[] { tenDollars }
+);
+
+var fiftyDong = new PhysicalAssetDeposit(
+    faceValue: 50m, 
+    currencyCode: "VND",
+    serialNumber: "IS16631076"
+);
+
+await service.RegisterStartupCapitalAsync(
+    targetAccountId: foundersCapitalAccountId,
+    assets: new[] { fiftyDong }
+);
+
+
+var twentyOSB = new PhysicalAssetDeposit(
+    faceValue: 1m, 
+    currencyCode: "OSB",
+    serialNumber: "395150299"
+);
+
+await service.RegisterStartupCapitalAsync(
+    targetAccountId: foundersCapitalAccountId,
+    assets: new[] { oneOSB }
+);
