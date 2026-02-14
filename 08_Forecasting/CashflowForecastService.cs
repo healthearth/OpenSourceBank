543c60a2c10c20a42f8497f845acc91d013d8be3
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 03:59:07 -0800

    Update CashflowForecastService.cs

--- 08_Forecasting/CashflowForecastService.cs
+++ 08_Forecasting/CashflowForecastService.cs
@@ -1,1 +1,15 @@
 //
+public class CashflowForecastService
+{
+    public List<decimal> Forecast(
+        List<decimal> historicalDailyTotals,
+        int daysAhead = 30)
+    {
+        var avg = historicalDailyTotals.Average();
+
+        return Enumerable
+            .Range(1, daysAhead)
+            .Select(d => avg * (1 + 0.01m * d))
+            .ToList();
+    }
+}
