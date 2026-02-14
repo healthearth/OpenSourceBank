659cd8630ca0394b5f56dcdc6f63fcddd2153e7a
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 08:01:36 -0800

    Create MetricsController.cs

---
+++ 04_Presentation_API/Controllers/MetricsController.cs
@@ -0,0 +1,22 @@
+[ApiController]
+[Route("api/[controller]")]
+public class MetricsController : ControllerBase
+{
+    private readonly MetricsService _metrics;
+
+    public MetricsController(MetricsService metrics)
+    {
+        _metrics = metrics;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new {
+            tps = _metrics.TransactionsPerSecond(),
+            fraud = _metrics.FraudCount(),
+            users = _metrics.ActiveUsers(),
+            volatility = _metrics.CurrencyVolatilityPercent()
+        });
+    }
+}
