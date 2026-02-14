512527349345949a1e94b0495ed38e0994ca0551
Health 3 the Earth <1akieckhefer@gmail.com>
2026-02-14 04:20:00 -0800

    Update User.cs

--- 01_Core_Domain/Entities/User.cs
+++ 01_Core_Domain/Entities/User.cs
@@ -1,1 +1,10 @@
 //
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    public List<Account> Accounts { get; set; } = new();
+}
